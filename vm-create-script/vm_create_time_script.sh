50229b97bf6d5ab68c1dcca5e484b39af525ecbf
Saqib Awan <saawan@trill.io>
2019-09-15 08:58:31 -0700

    Revert "removed certbot container"
    This reverts commit 936b1a0c

--- vm-create-script/vm_create_time_script.sh
+++ vm-create-script/vm_create_time_script.sh
@@ -14,23 +14,10 @@ sudo yum -y install epel-release
 sudo yum -y install -y python-pip
 sudo pip install docker-compose
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-#sudo yum -y install python36u
-#sudo yum -y install python36u-pip
-#sudo yum -y install python36u-devel
-#sudo yum -y upgrade python*
-sudo yum -y install htop
-#sudo yum -y install nginx
-#sudo systemctl start nginx
-#sudo firewall-cmd --permanent --zone=public --add-service=http
-#sudo firewall-cmd --permanent --zone=public --add-service=https
-#sudo firewall-cmd --reload
-#sudo systemctl enable nginx
-sudo yum -y install git
-sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-
-# cd /opt/letsencrypt
-# ./certbot-auto --nginx -d example.com -d www.example.com
-
-#sudo ln -s /bin/python3.6 /usr/local/bin/python3
+sudo yum -y install python36u
+sudo yum -y install python36u-pip
+sudo yum -y install python36u-devel
+sudo yum -y upgrade python*
+sudo ln -s /bin/python3.6 /usr/local/bin/python3
 
 sudo git clone https://github.com/opentrillo/install-centos.git /etc/trillo
